c00050b950a5973d8956f9d3709ea05398964b54
Misha Khrustavka <mishakhrustavka@mail.ru>
2018-04-23 23:08:20 +0300

    first commit

---
+++ wsh2.js
@@ -0,0 +1,55 @@
+
+var args=WScript.Arguments;
+var excel='D:\ex.xlsx';
+var word='D:\word.docx';
+
+var XL = WScript.CreateObject("Excel.Application");
+var WD = WScript.CreateObject("Word.Application");
+// робимо вікно видимим і створюємо робочу книгу
+var text="";
+XL.Visible = true;
+WD.Visible = true;
+XL.WorkBooks.Add();
+//.Echo("Enter text or 'Quit' to Quit");
+var i=0;
+if (WScript.CreateObject("Scripting.FileSystemObject").FileExists(word))
+{
+	var WDDoc=WD.Documents.Open(word);
+	
+	WScript.Echo("Enter text or 'Quit' to Quit");
+	var i=0;
+	while(text!="Quit")
+	{
+		text=WScript.StdIn.ReadLine();
+		if (text!="Quit") 
+		{
+			WDDoc.Content.InsertAfter(text);
+			XL.Cells(1, ++i).Value =text;
+		}
+
+	}
+	WDDoc.Save();/////////////////////////////////////////////
+	
+	WDDoc.Close();
+	WD.Quit();
+	//beda
+	//XL.Save();
+	//XL.ActiveWindow.Close();
+	XL.Quit();
+	//SOME COMMENT FOR GIT
+}
+////////////////////////////////////////////////////////
+				 
+   
+
+
+
+
+
+
+
+
+
+
+
+
